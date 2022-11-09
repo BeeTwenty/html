609e34b491bc833e0d969d8325d6664ae723068e
beetwenty <sindrebergee96@gmail.com>
2022-11-09 12:33:41 +0100

    test

--- js/serverstatus.js
+++
@@ -1,7 +0,0 @@
-function initServerData(serverIp,serverPort){ 
-    // our function will take 2 parameters
-    console.log(serverIp);
-    console.log(serverPort);
-}
-
-initServerData("au11.no","19133");
