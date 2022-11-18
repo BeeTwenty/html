14af8897b86c796e748b3e275a4b6e1c7fbbe4a9
beetwenty <sindrebergee96@gmail.com>
2022-11-18 09:47:00 +0100

    contact fix

--- contact.php
+++ contact.php
@@ -166,7 +166,17 @@ function validateForm() {
       return false;
   }
   document.querySelector('.status').innerHTML = "Sending...";
+  messageSend();
 }
+
+function messageSend(){
+    formData = {
+    'name'     : $('input[name=name]').val(),
+    'email'    : $('input[name=email]').val(),
+    'subject'  : $('input[name=subject]').val(),
+    'message'  : $('textarea[name=message]').val()
+};
+
 formData = {
   'name': $('input[name=name]').val(),
   'email': $('input[name=email]').val(),
