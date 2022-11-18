1a8b1e5fd8dea6a5f499dec4603d99ee910cf9c8
beetwenty <sindrebergee96@gmail.com>
2022-11-18 08:37:45 +0100

    fixed error

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 <link rel="script" href="/script.js">
 <link rel="icon" href="img/favicons/favicon.png.jpg">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+<?php include("mail.php")?>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
